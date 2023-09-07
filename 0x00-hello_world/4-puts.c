0ed6061f21a68ea71929623ce050d5e1017e2e7d
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-07 15:43:46 -0700

    sdasd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * Programming is like building a multilingual puzzle,
+ * main - a  Programming is like building a multilingual puzzle,
+ * return: 0 (winning or s success)
  */
 int main(void)
 {
