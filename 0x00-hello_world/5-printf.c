bb329244a417887518ac824485d01440226ecfd1
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-07 16:02:12 -0700

    asasd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  *  main - printing without thinking on outcomes
  *  Return: 0 (pass)
- *  */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 } 
