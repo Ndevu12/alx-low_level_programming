410b725c36dce58b94153ed32aeb6e13603d125f
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-07 16:55:36 -0700

    as

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *  main - printing without thinking on outcomes
- *  Return: 0 (pass)
+ *  Return: 0 (success)
  */
 int main(void)
-}
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
