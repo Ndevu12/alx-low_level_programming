60c92dfef99be68a80acb45a1865947a07f96213
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-07 16:19:56 -0700

    sdfs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  *  main - printing without thinking on outcomes
  *  Return: 0 (pass)
  */
-
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-} 
+}
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
