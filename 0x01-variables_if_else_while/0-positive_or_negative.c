f523a99050748c0dced3a92e05fafb5abf84d908
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-08 08:01:32 -0700

    dfd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  *  main - Entry point
- *  
+ *
  *  Return: Always 0 (Success)
  */
 int main(void)
@@ -15,7 +15,7 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if(n == 0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
