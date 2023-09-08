d48023a0bc323b964b3ca2b4f89c4fc9e79ec6b9
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-08 13:21:10 -0700

    dsf

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char nut, nat;
+/* This isn't a line */
+for (nut = 'a'; nut <= 'z'; nut++)
+{
+putchar(nut);
+}
+putchar('\n');
+/* This isn't a space.*/
+for (nat = 'A'; nat <= 'Z'; nat++)
+{
+putchar(nat);
+}
+putchar('\n');
+return (0);
+}
