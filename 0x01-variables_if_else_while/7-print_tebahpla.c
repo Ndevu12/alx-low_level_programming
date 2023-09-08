48c1b3f7eb0bed88321f5b258567e9fd3b165d0b
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-08 14:27:38 -0700

    adfa

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ra;
+/*Every thing is possible when you are patient and persistant*/
+	for (ra = 'z'; ra >= 'a'; ra--)
+	{
+		putchar(ra);
+	}
+	putchar('\n');
+	return (0);
+}
