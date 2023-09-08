d8aba71affafb71baf29d81d36c9685038dbbc64
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-08 11:10:17 -0700

    sdws

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,13 @@ if (last > 0)
 {
 	printf("Last digit of %d is %d and is greater than 5", n, last);
 }
-else if (n == 0)
+else if (n < 6)
 {
-	printf("Last digit of %d is %d and is 0", n, last);
+	printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
+	printf("Last digit of %d is %d and is 0", n, last);
 }
 return (0);
 }
