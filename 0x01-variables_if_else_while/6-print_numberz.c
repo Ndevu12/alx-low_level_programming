fa0b683345065ade650422d17a3ab692b32d10bb
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-08 14:18:54 -0700

    asdas

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nu;
+/*This means nothing about this code*/
+	for (nu = 0; nu <= 9; nu++)
+	{
+		putchar('0' + nu);
+	}
+	putchar('\n');
+	return (0);
+}
