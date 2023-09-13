582a6370ba588182a02a76dc91a584db0f322a94
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-13 04:14:13 -0700

    updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,29 +6,31 @@
  */
 void jack_bauer(void)
 {
-	int i, j, c, d;
-	/*let us print these minutes using for loop*/
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <=  9; j++)
-		{
-			if ((i <= 2 && j <= 9) || (i <= 2 && j <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						if (i == 2 && j > 3)
-							break;
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
-		}
-	}
+int i, j, c, d;
+/*let us print these minutes using for loop*/
+for (i = 0; i <= 2; i++)
+{
+for (j = 0; j <=  9; j++)
+{
+if ((i <= 2 && j <= 9) || (i <= 2 && j <= 3))
+{
+for (c = 0; c <= 5; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+if (i == 2 && j > 3)
+{
+break;
+}
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(58);
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
 }
