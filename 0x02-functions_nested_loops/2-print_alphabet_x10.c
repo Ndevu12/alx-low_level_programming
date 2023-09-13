2a181aba1265b6ea5223cd56a0ae3f8c61b036b1
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-13 07:18:23 -0700

    10 times the alphabet,

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /***
  * print_alphabet_x10 - Aimed at printing alphabet in ten times
+ * @i: charactor to be printed
+ * @j: charactor to used for loop
  * Return: nothing
  */
 void print_alphabet_x10(void)
