c6818dbfbaee2f6fec65632315a19486723b8076
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-18 14:19:50 -0700

    engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,25 @@
 #include "main.h"
-#include <string.h>
+
 /**
- * rev_string -  function that reverses a string
- * @s: string array
- * Return: 0 always
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
+
 void rev_string(char *s)
 {
-int i, k = 0, len = strlen(s);
-/*let us reverse this string*/
-for (i = len - 1; i >= 0; i--)
-{
-if (k <= len)
-{
-s[k] = s[i];
-k++;
+	char rev = s[0];
+	int co = 0;
+	int i;
+
+	while (s[co] != '\0')
+		co++;
+	for (i = 0; i < co; i++)
+	{
+		co--;
+		rev = s[i];
+		s[i] = s[co];
+		s[co] = rev;
+	}
 }
-}
-}
+
