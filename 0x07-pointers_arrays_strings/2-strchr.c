a4d194172f74aa7f78486f110e671ab6f8848f2b
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-09-25 04:54:36 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - pointing function 
+ * @s: input a
+ * @c: input b
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	return (0);
+}
+
