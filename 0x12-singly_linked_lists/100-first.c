548d33560f86f8841d89d59fd84eb7b2e6313ac1
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-10-19 10:32:04 -0700

    function executed before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * write_string - print out a string
+ * Return: nothing
+ */
+void write_string(void) __attribute__((constructor));
+
+
+/**
+ * write_string - print out a string
+ * Return: nothing
+ */
+
+void write_string(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
