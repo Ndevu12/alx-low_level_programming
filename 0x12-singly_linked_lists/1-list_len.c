f7d428a7af75ad339c90bb5a057790b09f1a3a5e
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-10-19 06:29:16 -0700

    meeting betty requirements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,13 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @count: count the number of elements
- * Return: Number of elements 
+ * @h: count the number of elements
+ * Return: Number of elements
  */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		count++;
